0768ac968862dc3c51407b6b73d0bfb3fc13dd4f
Alexander Kim <alexander.kim@ualberta.ca>
2019-04-22 00:48:19 -0400

    update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 params = dict()
 params['num_classes'] = 101
-params['dataset'] = '/home/akim/datasets/UCF-101'
+params['dataset'] = ''
 params['epoch_num'] = 40
 params['batch_size'] = 16
 params['step'] = 10
@@ -9,7 +9,7 @@ params['learning_rate'] = 3e-3
 params['momentum'] = 0.9
 params['weight_decay'] = 1e-5
 params['display'] = 10
-params['pretrained'] = 'UCF101/2019-04-21-09-42-18/clip_len_64frame_sample_rate_1_checkpoint_13.pth.tar'
+params['pretrained'] = None
 params['gpu'] = [0]
 params['log'] = 'log'
 params['save_path'] = 'UCF101'
